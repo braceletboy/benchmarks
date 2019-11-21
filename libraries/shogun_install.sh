57b523e3cf811980546e19a060968d793143085f
braceletboy <rukman.sai2@gmail.com>
2019-11-21 11:26:10 +0530

    Make script install system's py version of shogun
    Shogun's script install python3.5 interface for shogun. Now it's been
    generalized to install for systems python3 version.
    
    Changes to be committed:
    	modified:   libraries/shogun_install.sh

--- libraries/shogun_install.sh
+++ libraries/shogun_install.sh
@@ -7,21 +7,21 @@
 #
 # One shogun.tar.gz file should be located in this directory. The first argument
 # is the number of cores to use for build.
-if [ "$1" -eq "" ]; then
+if [ "$1" == "" ]; then
   cores="1";
 else
   cores="$1";
 fi
 
 tars=`ls shogun.tar.gz | wc -l`;
-if [ "$tars" -eq "0" ];
+if [ "$tars" == "0" ];
 then
   echo "No source shogun.tar.gz found in libraries/!"
   exit 1
 fi
 
 gpl_tars=`ls shogun-gpl.tar.gz | wc -l`;
-if [ "$gpl_tars" -eq "0" ];
+if [ "$gpl_tars" == "0" ];
 then
     echo "No gpl source shogun-gpl.tar.gz found in libraries/!"
     exit 1
@@ -38,9 +38,11 @@ tar -xzpf shogun-gpl.tar.gz --strip-components=1 -C shogun/src/gpl
 cd shogun/
 mkdir build/
 cd build/
-cmake -DPYTHON_INCLUDE_DIR=/usr/include/python3.5 \
+PYVER=`python3 -c 'import sys; print("python" + sys.version[0:3])'`;
+mkdir -p ../../lib/$PYVER/dist-packages/;
+cmake -DPYTHON_INCLUDE_DIR=/usr/include/$PYVER \
     -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3 \
-    -DPYTHON_PACKAGES_PATH=../../lib/python3.5/dist-packages \
+    -DPYTHON_PACKAGES_PATH=../../lib/$PYVER/dist-packages \
     -DINTERFACE_PYTHON=ON \
     -DBUILD_META_EXAMPLES=OFF \
     -DCMAKE_BUILD_TYPE=Release \
